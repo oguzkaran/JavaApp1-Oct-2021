784d50ba1b07a31f5cca130ead591009956a4962
Oğuz Karan <oguzkaran@csystem.org>
2021-10-16 20:52:52 +0300

    Update

--- src/Projects/Sample/src/main/java/org/csystem/app/App.java
+++ src/Projects/Sample/src/main/java/org/csystem/app/App.java
@@ -1,19 +1,13 @@
 package org.csystem.app;
 
+import org.csystem.util.console.Console;
 import org.csystem.util.string.StringUtil;
-
-import java.util.Random;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args)
     {
-        Scanner kb = new Scanner(System.in);
-        Random r = new Random();
-
         for (;;) {
-            System.out.print("Bir sayı giriniz:");
-            int n = Integer.parseInt(kb.nextLine());
+            int n = Console.readInt("Bir sayı giriniz:", "Hatalı giriş yaptınız!...");
 
             if (n <= 0)
                 break;

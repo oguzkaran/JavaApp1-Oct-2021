93b78dc7fd7917c9f6f4dfe8ef8c78fe231af0c2
Oğuz Karan <oguzkaran@csystem.org>
2021-12-04 20:49:03 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,39 +1,36 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Apache commons StopWatch sınıfı
-    https://commons.apache.org/
-    Anahtar Notlar: Bu kütüphanedeki hemen hemen her sınıf Apache Lisansına göre kullanılmaktadır. Bu lisansı inceleyeniz:
-    link: http://www.apache.org/licenses/LICENSE-2.0 (28.11.2021)
+    "this expression"'ın genel biçimi:
+        <sınıf ismi>.this
+    şeklindedir. Bu durumda this expression ile kullanılan sınıf türünden referans elde edilmiş olur. Aşağıdaki
+    örnekte A.this diyerek B'nin içerisinde gizlice tutulan A referansına erişilmiş olur. Bu A referansı B nesnesinin
+    yaratılmasında kullanılan referanstır. Örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import org.apache.commons.lang3.time.StopWatch;
 import org.csystem.util.console.Console;
-import org.csystem.util.numeric.NumberUtil;
-
-import java.math.BigInteger;
-import java.util.concurrent.TimeUnit;
 
 class App {
     public static void main(String[] args)
     {
-        long n = 5697859706174583067L;
+        var x = new A();
+        A.B y = x.new B();
 
-        var stopWatch = new StopWatch();
-
-        stopWatch.start();
-        var result = NumberUtil.isPrime(n);
-        stopWatch.stop();
-        var elapsed = stopWatch.getTime(TimeUnit.MILLISECONDS);
-
-        Console.writeLine(result ? "Asal" : "Asal değil");
-        Console.writeLine("Elapsed (sec):%f", elapsed / 1000.);
-
-        stopWatch.reset();
-        stopWatch.start();
-        result = NumberUtil.isPrime(BigInteger.valueOf(n));
-        stopWatch.stop();
-        elapsed = stopWatch.getTime(TimeUnit.MILLISECONDS);
-        Console.writeLine(result ? "Asal" : "Asal değil");
-        Console.writeLine("Elapsed (sec):%f", elapsed / 1000.);
+        y.foo();
     }
+}
+
+class A {
+    //...
+    public void foo()
+    {
+        Console.writeLine("A.foo");
+    }
+
+    public class B {
+        public void foo()
+        {
+            A.this.foo();
+        }
+    }
+    //...
 }

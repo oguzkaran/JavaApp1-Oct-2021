c262e24d04bafd21a1b463174211a84f1862e534
Oğuz Karan <oguzkaran@csystem.org>
2021-12-05 20:36:12 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,36 +1,26 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    "this expression"'ın genel biçimi:
-        <sınıf ismi>.this
-    şeklindedir. Bu durumda this expression ile kullanılan sınıf türünden referans elde edilmiş olur. Aşağıdaki
-    örnekte A.this diyerek B'nin içerisinde gizlice tutulan A referansına erişilmiş olur. Bu A referansı B nesnesinin
-    yaratılmasında kullanılan referanstır. Örneği inceleyiniz
+    Basit bir dijital saat uygulaması
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 import org.csystem.util.console.Console;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
+
 class App {
     public static void main(String[] args)
     {
-        var x = new A();
-        A.B y = x.new B();
+        var prompt = Console.read("Mesajı giriniz:");
 
-        y.foo();
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            public void run()
+            {
+                Console.write("%s:%s\r", prompt, DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss").format(LocalDateTime.now()));
+            }
+        }, 0, 1000);
     }
 }
 
-class A {
-    //...
-    public void foo()
-    {
-        Console.writeLine("A.foo");
-    }
-
-    public class B {
-        public void foo()
-        {
-            A.this.foo();
-        }
-    }
-    //...
-}

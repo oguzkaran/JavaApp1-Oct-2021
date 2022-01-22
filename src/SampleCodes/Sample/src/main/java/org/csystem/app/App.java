314dce0747e8508aab3ff6f2d6606e355072acce
Oğuz Karan <oguzkaran@csystem.org>
2022-01-22 19:52:10 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,132 +1,44 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Metot referans çeşitleri:
-    1. static metot referansı (static method reference)
-    2. Bir nesneye ilişkin non-static metot referansı (reference to an instance method of particular object)
-    3. Bir türe ilişkin non-static metot referansı (reference to an instance method of any object of a particular type)
-    4. ctor referansı (reference to constructor)
+    Java 8 ile birlikte java.util.function paketine bir grup fonksiyonel arayüz eklenmiştir. Bu arayüzler özellike yine
+    Java 8 ile eklenen "Stream API" tarafından kullanılmaktadır. Stream API Java'da programlama modelini geliştirmiştir ve
+    kullanılması uygun olan durumda kesinlikle kullanılmalıdır. Bu konu ileride detaylı biçimde ele alınacaktır. Şüphesiz
+    bu arayüzler yalnızca Stream API ile kullanılmaz. Programcı kendi tasarladığı sınıflarda ve metotlarda da kullanabilir.
+    Ayrıca Java 8 ile eklenen bu arayüzler başka sınıflar tarafından da kullanılmaktadır.
 
-    Aşağıdaki örnekte referanslara atama şüphesiz metot parametreleri için de geçerlidir. Tamamen örnek amaçlı yazılmıştır
+    java.util.function paketi içerisinde bulunan bu arayüzler basitçe şu şekilde gruplanabilir:
+    1. Function arayüzleri: Bu arayüzler bir ya da iki tane giriş alan ve bir sonuç üretilmesi işleminde kullanılır. Bu
+    arayüzlerin en geneli Function ve BiFunction arayüzleridir. Function<T, R> arayüzü T türünden bir değer alan ve R
+    türünden bir geri dönüş değerine sahip apply isimli bir metoda sahiptir. BiFunction<T, U, R> arayüzünde ise T ve U türünden
+    iki tane parametresi olan ve R türünden  geri dönüş değerine sahip apply isimli abstract metot vardır.
+
+    2. Operatör arayüzleri: Bu arayüzler bşr ya da iki girişe göre bir işlem yapan ve sonuç üreten durumlarda kullanılır.
+    Bunların birçoğu Function veya BiFunction arayüzünden türetilmiştir. Bu arayüzler de BinaryOperator ve UnaryOperator
+    olarak iki gruba ayrılır.Bu arayüzler bir ya da iki operandlı işlemler için kullanılır. Bu arayüzlerinde temel bazı
+    türler için ayrı karşılıkları da bulunur.
+
+    3. Supplier arayüzleri: Bu arayüzler bir giriş almadan bir değer üretmez için kullanılır
+    4. Consumer arayüzleri:
+    5. Predicate arayüzleri:
+    6. Geri kalanlar:
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 import org.csystem.util.console.Console;
-import org.csystem.util.numeric.NumberUtil;
+import org.csystem.util.string.StringUtil;
 
 import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class App {
     public static void main(String[] args)
     {
         Random r = new Random();
 
-        IIntBinaryOperator ibo = IntOperation::multiply; //1 -> Lambda karşılığı: (a, b) -> IntOperation.multiply(a, b);
+        IntStream.generate(r::nextInt).limit(10).filter(val -> val % 2 == 0).forEach(Console::writeLine);
 
-        Console.writeLine(ibo.apply(10, 20));
-        Console.writeLine("----------------------------------------------------");
-
-        IIntPredicate pred = NumberUtil::isPrime; //1 -> Lambda karşılığı: a -> NumberUtil.isPrime(a);
-
-        Console.writeLine(pred.test(1_000_003));
-        Console.writeLine("----------------------------------------------------");
-
-        IIntSupplier intSupplier = r::nextInt; //2 -> Lambda karşılığı: () -> r.nextInt();
-        Console.writeLine(intSupplier.get());
-        Console.writeLine("----------------------------------------------------");
-
-        IIntRandomBoundedSupplier ibs = Random::nextInt; //3 -> Lambda karşılığı: (r, b) -> r.nextInt(bound);
-
-        Console.writeLine(ibs.get(r, 100));
-        Console.writeLine("----------------------------------------------------");
-
-        IStringToIntConverter  sti = String::length; //3 -> Lambda karşılığı: s -> s.length();
-
-        Console.writeLine(sti.convert("csd"));
-        Console.writeLine("----------------------------------------------------");
-
-        IFunction<String, Integer> fsti = String::length; //3 -> Lambda karşılığı: s -> s.length(); //auto boxing
-
-        int val = fsti.apply("csd"); //auto-boxing
-
-        Console.writeLine(val);
-        Console.writeLine("----------------------------------------------------");
-
-        IFactory<Integer, IntValue> f = IntValue::new; //4 -> Lambda karşılığı: a -> new IntValue(a);
-
-        IntValue ival = f.create(20);
-
-        Console.writeLine(ival.getVal());
-        Console.writeLine("----------------------------------------------------");
-
-        IDefaultFactory<IntValue> fd = IntValue::new;
-
-        ival = fd.create();
-
-        Console.writeLine(ival.getVal());
-        Console.writeLine("----------------------------------------------------");
+        Console.writeLine("-------------------------------------");
+        Stream.generate(() -> StringUtil.getRandomTextTR(r, 10)).limit(10).forEach(Console::writeLine);
     }
 }
 
-class IntOperation {
-    //...
-    public static int multiply(int a, int b)
-    {
-        return a * b;
-    }
-}
-
-class IntValue {
-    private int m_val;
-
-    public IntValue()
-    {
-
-    }
-
-    public IntValue(int val)
-    {
-        m_val = val;
-    }
-
-    public int getVal()
-    {
-        return m_val;
-    }
-
-    //...
-}
-
-interface IFactory<T, R> {
-    R create(T t);
-}
-
-interface IDefaultFactory<R> {
-    R create();
-}
-
-interface IIntRandomBoundedSupplier {
-    int get(Random r, int bound);
-}
-
-interface IIntRandomSupplier {
-    int get(Random r);
-}
-
-interface IIntSupplier {
-    int get();
-}
-
-interface IIntPredicate {
-    boolean test(int val);
-}
-
-interface IStringToIntConverter {
-    int convert(String s);
-}
-
-interface IFunction<T, R> {
-    R apply(T t);
-}
-
-interface IIntBinaryOperator {
-    int apply(int a, int b);
-}

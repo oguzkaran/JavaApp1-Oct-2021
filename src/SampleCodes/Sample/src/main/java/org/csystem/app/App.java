1b585bbf35024719da4e46ed5b0d6e96160f74e4
Oğuz Karan <oguzkaran@csystem.org>
2022-02-27 20:19:16 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,23 +1,41 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Aşağıdaki örnekte Stream API kullanılarak stdin'den girilen stok miktarından daha çok olan ürünlerin isimleri
-    büyük harfe dönüştürülerek stout'a yazdırılmıştır. Örnek sadece genel Stream API kullanımını göstermek için yazılmıştır.
-    Detaylar konu içerisinde ele alınacaktır
+    Stream arayüzlerinin reduce metodu
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
+import org.csystem.app.data.factory.NumberFactory;
 import org.csystem.util.console.Console;
+import org.csystem.util.console.command.CommandLineUtil;
+import org.csystem.util.numeric.NumberUtil;
 
-import java.util.Random;
+import java.nio.file.Path;
 import java.util.stream.IntStream;
 
 class App {
     public static void main(String[] args)
     {
-        var n = Console.readInt("Bir sayı giriniz:");
-        var random = new Random();
+        try {
+            CommandLineUtil.checkForLengthEqual(args, 1, "Wrong number of arguments", 1);
+            var factory = NumberFactory.loadFromTextFile(Path.of(args[0]));
+            var numbers = factory.getNumbers();
 
-        IntStream.generate(() -> random.nextInt(100)).limit(n).forEach(val -> Console.write("%d ", val));
-        Console.writeLine();
+            var value = Console.readInt("Input a number:");
+
+            IntStream.of(numbers)
+                    .filter(a -> a > value)
+                    .filter(NumberUtil::isPrime)
+                    .forEach(a -> Console.write("%d ", a));
+            Console.writeLine();
+
+            var result = IntStream.of(numbers)
+                    .filter(a -> a > value)
+                    .filter(NumberUtil::isPrime)
+                    .reduce(1, (r, a) -> r * a);
+
+            Console.writeLine("Total:%d", result);
+        }
+        catch (Throwable ex) {
+            ex.printStackTrace();
+        }
     }
 }
-

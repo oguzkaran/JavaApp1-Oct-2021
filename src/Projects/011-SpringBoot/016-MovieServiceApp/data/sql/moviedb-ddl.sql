ba0ebc8da77f87c3cfe199ae81bd325ea059ef45
Oğuz Karan <oguzkaran@csystem.org>
2022-04-21 17:02:57 +0300

    Update

--- src/Projects/011-SpringBoot/016-MovieServiceApp/data/sql/moviedb-ddl.sql
+++ src/Projects/011-SpringBoot/016-MovieServiceApp/data/sql/moviedb-ddl.sql
@@ -1,26 +1,24 @@
--- japp1o21_moviesappdb
+-- intellica_movieappdb
 
 create table movies (
     movie_id bigserial primary key,
     name varchar(256) not null,
     scene_date date not null,
-    rating bigint default(0) not null,
+    rating bigint not null default(0) check(rating >= 0),
     cost real not null,
-    imdb real
+    imdb float4 not null default(0.0)
 );
 
-
 create table directors (
-    email varchar(50) primary key,
+    director_id bigserial primary key,
     first_name varchar(100) not null,
     middle_name varchar(100),
     family_name varchar(100) not null,
     birth_date date not null
 );
 
-create table movies_to_directors (
-    movies_to_directors_id bigserial primary key,
+create table movies_to_director (
+    movies_to_director_id bigserial primary key,
     movie_id bigint references movies(movie_id),
-    email varchar(50) references directors(email),
-    register_date date  default(current_date) not null
+    director_id bigint references directors(director_id)
 );
